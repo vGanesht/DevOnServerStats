0ae4e4850d1180f0ca3c29e5f6cf45bc8ba40178
Vidya Ganesh <vidya.t52@outlook.com>
2021-06-06 08:51:22 +0530

    added overfflow for table for lower screen resolution

--- src/app/dashboard/filters/filter-constants.ts
+++ src/app/dashboard/filters/filter-constants.ts
@@ -7,4 +7,4 @@ export const DEFAULT_FILTER_DATA={
 export const STORAGE_DETAILS=["0", "250GB", "500GB", "1TB", "2TB", "3TB", "4TB", "8TB", "12TB", "24TB", "48TB", "72TB"];
 
 export const RAM_SPECS_LIST= ['2GB', '4GB', '8GB', '12GB', '16GB', '24GB', '32GB', '48GB', '64GB', '96GB'];
-export const HDD_SPEC_LIST=['SAS', 'SATA', 'SSD'];
+export const HDD_SPEC_LIST=['SAS', 'SATA2', 'SSD'];
